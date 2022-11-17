b803e60be77709b0f846231e36ad0ef505a6bbbf
yoko <patesie.5sy8.ribon4@gmail.com>
2022-11-18 00:26:59 +0900

    クイズアプリを作りました”

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,29 +1,62 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ "クイズタイトル" }}</h2>
+    <h2>Q. {{ quiz.text }}</h2>
     <img
       class="quiz-image"
       src="https://via.placeholder.com/300x300"
-      alt="クイズタイトル"
+      v-bind:alt="quiz.text"
     />
     <div class="container">
-      <button>
-        {{ "選択肢1" }}
-      </button>
-      <button>
-        {{ "選択肢2" }}
-      </button>
-      <button>
-        {{ "選択肢3" }}
+      <button
+        v-for="(choice, i) in quiz.choices"
+        v-bind:key="i"
+        v-on:click="choiced(choice)"
+      >
+        {{ choice.text }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div>{{ feedback }}</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      feedback: "",
+      quiz: {
+        text: "1868年～1912年の元号はなんでしょう",
+        choices: [
+          {
+            text: "江戸",
+            isCorrect: false,
+            feedback:
+              "残念！江戸時代は1603年から1868年です",
+          },
+          {
+            text: "明治",
+            isCorrect: true,
+            feedback:
+              "正解！1868年から1912年は明治時代です",
+          },
+          {
+            text: "大正",
+            isCorrect: false,
+            feedback:
+            "残念！大正時代は1912年から1926年です",
+          },
+        ],
+      },
+    }
+  },
+ methods: {
+   choiced(choice){
+    this.feedback = choice.feedback
+   },
+ },
+}
+
 </script>
 
 <style>
