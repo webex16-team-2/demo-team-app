fcb0416c51e453e07eb091033ddb4e5f24b362de
yoko <patesie.5sy8.ribon4@gmail.com>
2022-11-19 00:15:34 +0900

    homeを作りました

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,59 @@
 <template>
   <h1>Home</h1>
-  <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <div class="firstblock">
+    <router-link
+        to="/quiz"
+        class="vuelink"
+        exact-active-class="vuelink--exact-active"
+        >Vue クイズ</router-link
+      >
+      </div>
+  <div class="secondblock">
+      <router-link
+        to="/palette"
+        class="vuelink"
+        exact-active-class="vuelink--exact-active"
+        >Vue パレット</router-link
+      >
+      </div>
+  <div class="thirdblock">
+      <router-link
+        to="/memo"
+        class="navigation__link"
+        exact-active-class="navigation__link--exact-active"
+        >Vue メモ</router-link
+      ></div>
 </template>
+
+<style scoped>
+.firstblock{
+  height: 300px;
+  background-color: #ddd;
+  font-size: 60px;
+  font-family: serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.secondblock {
+  height: 300px;
+  background-color: #f0f0f0;
+  font-size: 60px;
+  font-family: serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+
+.thirdblock {
+  height: 300px;
+  background-color: #f05f40;
+  font-size: 60px;
+  font-family: serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+</style>
