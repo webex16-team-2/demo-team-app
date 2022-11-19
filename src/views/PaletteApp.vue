15b73f5c7aac79c8b1829c68f97a5cc55ff8bfbd
masan894 <MasanaMail762@gmail.com>
2022-11-19 17:11:47 +0900

    パレットを作成しました

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -1,15 +1,69 @@
 <template>
-  <h1>Vue パレット</h1>
+  <h1>赤めのVueパレット</h1>
   <div class="app">
-    <div class="palette" style="background-color: rgba(0, 0, 200, 0.5)"></div>
-    <p>rgba( {{ 0 }}, {{ 0 }}, 200, 0.5 )</p>
+    <div
+      class="palette"
+      v-on:mousemove="changeColor"
+      v-on:click="fetchColor"
+      v-bind:style="palletStyle"
+    ></div>
+    <p>rgba( 150, {{ green }}, {{ blue }}, 0.5 )</p>
     <div class="colors-container">
-      <div class="mini-palette"></div>
+      <div
+        class="mini-palette"
+        v-for="color in colors"
+        :key="color.id"
+        v-bind:style="{
+          backgroundColor: `rgba(150, ${color.green},${color.blue}, 0.5)`,
+        }"
+        v-on:click="showColor(color)"
+      ></div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      green: 0,
+      blue: 0,
+      colors: [
+        //色のデータが並ぶ
+      ],
+    }
+  },
+  methods: {
+    changeColor(e) {
+      this.green = e.offsetX
+      this.blue = e.offsetY
+    },
+    fetchColor() {
+      const newColor = {
+        green: this.green,
+        blue: this.blue,
+      }
+      this.colors.push(newColor)
+    },
+    showColor(color) {
+      this.green = color.green
+      this.blue = color.blue
+    },
+  },
+  computed: {
+    palletStyle() {
+      return {
+        backgroundColor: `rgba(150,${this.green}, ${this.blue}, 0.5)`,
+      }
+    },
+  },
+}
+</script>
+
 <style>
+h1 {
+  text-align: center;
+}
 .app {
   display: flex;
   width: 100%;
